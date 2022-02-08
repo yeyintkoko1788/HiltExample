adddf9b2d871db6656c8fa05cdee3af577fd7cdd
Ye Yint Ko Ko <yeyintkoko.1788@gmail.com>
2022-02-08 06:30:06 -0800

    update

--- app/src/main/java/com/yeyint/hiltexample/utils/Constant.kt
+++ app/src/main/java/com/yeyint/hiltexample/utils/Constant.kt
@@ -2,11 +2,11 @@ package com.yeyint.hiltexample.utils
 
 class Constant {
     companion object{
-        const val BASE_URL = "https://ballsayar.com.mm/"
-        const val START_IMAGE_URL = "https://ballsayar.com.mm/storage"
+        const val BASE_URL = "***************"
+        const val START_IMAGE_URL = "**************"
 
         const val API_GET_TRENDING_POST = "api/get-trending-posts"
 
-        const val BASIC_AUTH_VALUE = "Basic YmFsbHNheWFyLm1tbDpiYWxsc2F5YXJAMjAyMQ=="
+        const val BASIC_AUTH_VALUE = "**************************"
     }
 }
